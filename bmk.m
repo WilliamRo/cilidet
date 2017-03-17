e00d6f8f00d137f559462d89550b1367772449fc
William <willi4m@zju.edu.cn>
2017-03-17 09:50:42 +0800

    [M] bmk

--- bmk.m
+++ bmk.m
@@ -3,7 +3,19 @@ function filename = bmk(index)
 %   ...
 
 if nargin < 1, index = 1; end
-filename = sprintf('data/benchmark/%d.tif', index);
+if ischar(index)
+    filename = ['data/', index];
+elseif index > 0, filename = sprintf('data/benchmark/%d.tif', index);
+else 
+    switch index
+        case 0
+            filename = 'data/dnc.tif';
+        case -1
+            filename = 'data/dnc.tif';
+        otherwise
+            error('!! No file found.')
+    end
+end
 
 end
 
